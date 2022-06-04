40a8f46818df4e4f692672852fdc9cf17e209004
mouse0429 <mouse0429@naver.com>
2022-06-05 00:16:30 +0900

    error fix
    error fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './style/Global.css';
-import './style/Notice.css'
-import MessagePage from './pages/Message/MessagePage';
 import Notice from './pages/Notice/NoticePage';
 import NoticeRead from './pages/Notice/NoticeRead';
 import NoticeWrite from './pages/Notice/NoticeWrite';
 import NoticeSearch from './pages/Notice/NoticeSearch';
-import BUY from './page/Search/BuyMain';
-import BUYSEARCH from './page/Search/BuySearch';
-import BUYDETAIL from './page/Detail/BuyDetail';
-import SELL from './page/Search/SellMain';
-import SELLSEARCH from './page/Search/SellSearch';
-import SELLDETAIL from './page/Detail/SellDetail';
-import MAIN from './page/Main/Main';
+import BUY from './pages/Search/BuyMain';
+import BUYSEARCH from './pages/Search/BuySearch';
+import BUYDETAIL from './pages/Detail/BuyDetail';
+import SELL from './pages/Search/SellMain';
+import SELLSEARCH from './pages/Search/SellSearch';
+import SELLDETAIL from './pages/Detail/SellDetail';
+import MAIN from './pages/Main/Main';
 
 function App() {
   return (
@@ -26,7 +24,6 @@ function App() {
         <Route path='/sell' element={<SELL/>}/>
         <Route path='/sell/search/*' element={<SELLSEARCH/>}/>
         <Route path="/sell/detail/*" element={<SELLDETAIL/>}/>
-        <Route path="/msgbox" element={<MessagePage/>}/>
         <Route path="/notice" element={<Notice/>}/>
         <Route path="/notice/read/*" element={<NoticeRead/>}/>
         <Route path="/notice/write" element={<NoticeWrite/>}/>
