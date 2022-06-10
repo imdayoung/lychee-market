5bf1bdf4942747bfcda641ff08bfe75ab61590b4
승윤 <89008921+sy0223lee@users.noreply.github.com>
2022-06-10 21:29:26 +0900

    Feature/report board (#29)
    * Merge pull request #22 from Jae211/feature/ReportBoard
    
    Feature/report board
    
    * 이미지 추가, 게시물 이동 연결
    
    * <tr>-<Link> 오류 수정

--- src/pages/Notice/components/NoticeListComponent.js
+++ src/pages/Notice/components/NoticeListComponent.js
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NoticeListComponent(props){
-    return (
-        <tr className="n_list_row">
-            <td className="noticeIndex">{props.notice_id}</td>
-            <Link to={{pathname: '/notice/read/'+props.notice_id}}><td className="noticeTitle">{props.notice_title}</td></Link>
-            <td className="noticeDate">{props.notice_date}</td>
-            <td className="managerId">{props.manager_id}</td>
-        </tr>
-    )
+  let Navigate = useNavigate();
+
+  const ToNoticeRead = ()=>{
+    Navigate("/notice/read/"+props.notice_id);
+  };
+  return (
+    <tr className="n_list_row">
+      <td className="noticeIndex">{props.notice_id}</td>
+      <td className="noticeTitleToRead"  onClick={ToNoticeRead}>{props.notice_title}</td>
+      <td className="noticeDate">{props.notice_date}</td>
+      <td className="managerId">{props.manager_id}</td>
+    </tr>
+  )
 }
--- src/pages/Report/components/ReportListComponent.js
+++ src/pages/Report/components/ReportListComponent.js
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function ReportListComponent(props){
+  let Navigate = useNavigate();
+
+  const ToReportDetail = ()=>{
+    Navigate("/report/detail/"+props.report_id);
+  };
+
   return (
     <tr className="ReportListRow">
       <td className="ReportIndex">{props.report_id}</td>
       <td className="ReportType">{props.report_type}</td>
-      <Link to={{pathname:'/report/detail/'+props.report_id}}><td className="ReportTitle">{props.report_title}</td></Link>
+      <td className="ReportTitleToDetail" onClick={ToReportDetail}>{props.report_title}</td>
       <td className="ReportDate">{props.report_date}</td>
       <td className="EsSolved">{props.is_solved}</td>
     </tr>
