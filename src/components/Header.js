c37a43a802790885ca68aba413e47fbeb417ee12
mouse0429 <mouse0429@naver.com>
2022-06-11 22:58:32 +0900

    fix Header
    fix Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
-import '../style/Header.css';
+import "../style/Header.css";
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import getCookie from './GetCookie';
-import setCookie from './SetCookie';
+import getCookie from "./GetCookie";
+import setCookie from "./SetCookie";
 
-export default function Header(props){
+export default function Header(props) {
   let Location = useLocation();
 
   useEffect(() => {
-      console.log('location', Location);
+    console.log("location", Location);
   }, [Location]);
-  const DealType = Location.pathname.split('/').slice(1)[0];
+  const DealType = Location.pathname.split("/").slice(1)[0];
   console.log(DealType);
 
-  const [Target, SetTarget] = useState('');
-  
+  const [Target, SetTarget] = useState("");
+
   const cookie = getCookie("is_login");
-  if(cookie === "true"){
+  if (cookie === "true") {
     // const tempid = localStorage.getItem("user_id");
     // alert(tempid);
     //로그인 없애고 로그아웃 나타내기
@@ -25,39 +25,62 @@ export default function Header(props){
   const onLogoutClick = () => {
     setCookie("is_login", true, -1);
     localStorage.clear();
-  }
+  };
 
-  const {keyword} = props;
-  let exist = false;
-  
-  if(keyword !== undefined){
-    exist = true;
-  }
+  // const {keyword} = props;
+  // let exist = false;
 
-  return(
+  // if(keyword !== undefined){
+  //   exist = true;
+  // }
+
+  return (
     <header>
       <nav className="Top">
-        <Link to={{pathname:'/login'}} id='LinkNoLine'><div className="TopNav">로그인</div></Link>
-        <Link to={{pathname:'/register'}} id='LinkNoLine'><div className="TopNav">회원가입</div></Link>
-        <Link to={{pathname:'/qna'}} id='LinkNoLine'><div className="TopNav">문의사항</div></Link>
-        <Link to={{pathname:'/notice'}} id='LinkNoLine'><div className="TopNav">공지사항</div></Link>
+        <Link to={{ pathname: "/login" }} id="LinkNoLine">
+          <div className="TopNav">로그인</div>
+        </Link>
+        <Link to={{ pathname: "/register" }} id="LinkNoLine">
+          <div className="TopNav">회원가입</div>
+        </Link>
+        <Link to={{ pathname: "/qna" }} id="LinkNoLine">
+          <div className="TopNav">문의사항</div>
+        </Link>
+        <Link to={{ pathname: "/notice" }} id="LinkNoLine">
+          <div className="TopNav">공지사항</div>
+        </Link>
       </nav>
-      
+
       <div className="Divider"></div>
 
       <div className="Title">
         <div className="TitleName">
-          <Link to={{pathname:'/'}} id='LinkNoLine'><div id="Logo"></div></Link>
+          <Link to={{ pathname: "/" }} id="LinkNoLine">
+            <div id="Logo"></div>
+          </Link>
           <div id="TitleText2">리치마켓</div>
-          {exist ? <div id="TitleDetail2"> | {keyword}</div> : <></>}
+          {props.keyword === undefined ? (
+            <></>
+          ) : (
+            <div id="TitleDetail2">{" | " + props.keyword}</div>
+          )}
+          {/* {exist ? <div id="TitleDetail2"> | {keyword}</div> : <></>} */}
         </div>
         <nav className="Top">
-          <Link to={{pathname:'/sell'}} id='LinkNoLine'><div className="TitleNav">판매</div></Link>
-          <Link to={{pathname:'/buy'}} id='LinkNoLine'><div className="TitleNav">구매</div></Link>
-          <Link to={{pathname: '/mypage'}} id='LinkNoLine'><div className="TitleNav">내정보</div></Link>
-          <Link to={{pathname: '/msgbox'}} id='LinkNoLine'><div className="TitleNav">쪽지함</div></Link>
+          <Link to={{ pathname: "/sell" }} id="LinkNoLine">
+            <div className="TitleNav">판매</div>
+          </Link>
+          <Link to={{ pathname: "/buy" }} id="LinkNoLine">
+            <div className="TitleNav">구매</div>
+          </Link>
+          <Link to={{ pathname: "/mypage" }} id="LinkNoLine">
+            <div className="TitleNav">내정보</div>
+          </Link>
+          <Link to={{ pathname: "/msgbox" }} id="LinkNoLine">
+            <div className="TitleNav">쪽지함</div>
+          </Link>
         </nav>
       </div>
     </header>
-  )
+  );
 }
--- src/pages/Notice/NoticeWrite.js
+++ src/pages/Notice/NoticeWrite.js
@@ -1,99 +1,134 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Axios from 'axios';
-import moment from 'moment';
-import Header from "../../components/Header"
+import Axios from "axios";
+import moment from "moment";
+import Header from "../../components/Header";
 import "../../style/Notice.css";
 
-export default function NoticeWrite(){
-    let Navigate = useNavigate();
+export default function NoticeWrite() {
+  let Navigate = useNavigate();
 
-    // manager_id 받아와야함
-    var ManagerId = "admin1"
-    // const [ManagerId, SetManagerId] = useState('admin1');
+  // manager_id 받아와야함
+  var ManagerId = "admin1";
+  // const [ManagerId, SetManagerId] = useState('admin1');
 
-    // 공지사항 작성 날짜 받아오기
-    var Now = moment();
-    var NoticeDate = Now.format('YYYY-MM-DD');
-    
-    // 공지사항 제목, 내용, 이미지
-    const [NoticeTitle, SetNoticeTitle] = useState('');
-    const [NoticeContent, SetNoticeContent] = useState('');
-    const [NoticeImg, SetNoticeImg] = useState('');
-    const [ImgName, SetImgName] = useState('');
+  // 공지사항 작성 날짜 받아오기
+  var Now = moment();
+  var NoticeDate = Now.format("YYYY-MM-DD");
 
-    // 공지사항 작성
-    const SubmitClick = ()=>{
-        if(ImgName!==''){
-            // 이미지 업로드를 위해 formdata 생성
-            const formData = new FormData();
-            formData.append('img', NoticeImg);
+  // 공지사항 제목, 내용, 이미지
+  const [NoticeTitle, SetNoticeTitle] = useState("");
+  const [NoticeContent, SetNoticeContent] = useState("");
+  const [NoticeImg, SetNoticeImg] = useState("");
+  const [ImgName, SetImgName] = useState("");
 
-            Axios.post("http://localhost:8080/upload/image", formData);
-        }        
+  // 공지사항 작성
+  const SubmitClick = () => {
+    if (ImgName !== "") {
+      // 이미지 업로드를 위해 formdata 생성
+      const formData = new FormData();
+      formData.append("img", NoticeImg);
 
-        Axios.post("http://localhost:8080/notice/write", {
-            manager_id: ManagerId,
-            notice_date: NoticeDate,
-            notice_title: NoticeTitle,
-            notice_content: NoticeContent,
-            notice_img: ImgName!==''?'/images/notice/'+ImgName:null,
-        }).then((res)=>{
-            console.log(res);
-            if(res.data === true){
-                alert("공지사항 업로드가 완료되었습니다.");
-                Navigate('/notice');
-            }
-            else{
-                alert("공지사항 업로드를 실패하였습니다.");
-                Navigate('/notice/write');
-            }
-        });
-    };
+      Axios.post("http://localhost:8080/upload/image", formData);
+    }
 
-    // 목록으로 돌아가기
-    const ListClick = ()=>{
-        if(window.confirm("공지사항 목록으로 돌아가시겠습니까?") === true){ 
-            Navigate('/notice');
-        }          
-    };
+    Axios.post("http://localhost:8080/notice/write", {
+      manager_id: ManagerId,
+      notice_date: NoticeDate,
+      notice_title: NoticeTitle,
+      notice_content: NoticeContent,
+      notice_img: ImgName !== "" ? "/images/notice/" + ImgName : null,
+    }).then((res) => {
+      console.log(res);
+      if (res.data === true) {
+        alert("공지사항 업로드가 완료되었습니다.");
+        Navigate("/notice");
+      } else {
+        alert("공지사항 업로드를 실패하였습니다.");
+        Navigate("/notice/write");
+      }
+    });
+  };
 
-    return (
-        <div>
-            <Header detail='공지사항'/>
-            <main className="noticeMain">
-                <table className="noticeWrite">
-                    <tbody>
-                        <tr className="n_row">
-                            <th className="n_th">제목</th>
-                            <td><input className="n_td" type='text' onChange={(e) => SetNoticeTitle(e.target.value)}/></td>
-                        </tr>
-                        <tr className="n_row">
-                            <th className="n_th">작성 날짜</th>
-                            <td><input className="n_td" type='text' value={NoticeDate} readOnly/></td>
-                        </tr>
-                        <tr className="n_row">
-                            <th className="n_th">작성자</th>
-                            <td><input className="n_td" type='text' value={ManagerId} readOnly/></td>
-                        </tr>
-                        <tr className="n_row">
-                            <th className="n_th">내용</th>
-                            <td>
-                                <textarea className="n_td_content" onChange={(e) => SetNoticeContent(e.target.value)}>
-                                </textarea>
-                            </td>
-                        </tr>
-                        <tr className="n_row">
-                            <th className="n_th">이미지</th>
-                            <td><input className="n_td_image" type="file" accept="image/png, image/jpeg" onChange={(e) => {SetNoticeImg(e.target.files[0]); SetImgName(e.target.files[0].name)}}/></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div className="buttons">
-                    <button className="list" type="button" onClick={ListClick}>목록</button>
-                    <button className="submit" type="button" onClick={SubmitClick}>작성하기</button>
-                </div>
-            </main>
+  // 목록으로 돌아가기
+  const ListClick = () => {
+    if (window.confirm("공지사항 목록으로 돌아가시겠습니까?") === true) {
+      Navigate("/notice");
+    }
+  };
+
+  return (
+    <div>
+      <Header keyword="공지사항" />
+      <main className="noticeMain">
+        <table className="noticeWrite">
+          <tbody>
+            <tr className="n_row">
+              <th className="n_th">제목</th>
+              <td>
+                <input
+                  className="n_td"
+                  type="text"
+                  onChange={(e) => SetNoticeTitle(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr className="n_row">
+              <th className="n_th">작성 날짜</th>
+              <td>
+                <input
+                  className="n_td"
+                  type="text"
+                  value={NoticeDate}
+                  readOnly
+                />
+              </td>
+            </tr>
+            <tr className="n_row">
+              <th className="n_th">작성자</th>
+              <td>
+                <input
+                  className="n_td"
+                  type="text"
+                  value={ManagerId}
+                  readOnly
+                />
+              </td>
+            </tr>
+            <tr className="n_row">
+              <th className="n_th">내용</th>
+              <td>
+                <textarea
+                  className="n_td_content"
+                  onChange={(e) => SetNoticeContent(e.target.value)}
+                ></textarea>
+              </td>
+            </tr>
+            <tr className="n_row">
+              <th className="n_th">이미지</th>
+              <td>
+                <input
+                  className="n_td_image"
+                  type="file"
+                  accept="image/png, image/jpeg"
+                  onChange={(e) => {
+                    SetNoticeImg(e.target.files[0]);
+                    SetImgName(e.target.files[0].name);
+                  }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="buttons">
+          <button className="list" type="button" onClick={ListClick}>
+            목록
+          </button>
+          <button className="submit" type="button" onClick={SubmitClick}>
+            작성하기
+          </button>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
