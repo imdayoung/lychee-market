e37bbc497ec50df40e1359df1434b864a3d1296e
mouse0429 <mouse0429@naver.com>
2022-06-12 19:28:41 +0900

    report number chart complete
    report number chart complete

--- src/pages/Management/ManageMain.js
+++ src/pages/Management/ManageMain.js
@@ -58,6 +58,40 @@ export default function ManageMain() {
         console.log("신규 가입자 차트 에러");
       });
     
+    axios.post("http://localhost:8080/chart/report", {
+      Date: Common.GetSixDaysAgo(),
+    })
+    .then((res)=>{
+      var resultData = [0, 0, 0, 0, 0, 0, 0];
+      for (var i = 0; i < res.data.length; i++) {
+        resultData[
+          Common.GetDateGap(res.data[i].report_date, Common.GetSixDaysAgo())
+        ] = res.data[i].cnt;
+      }
+      SetReportNum({
+        labels: Common.GetWeek(),
+        datasets: [
+          {
+            type: "bar",
+            label: "이번 주 신고 현황",
+            borderColor: "#ff3d60",
+            backgroundColor: "#ff3d60",
+            borderWidth: 2,
+            data: resultData,
+          },
+        ],
+      });
+    })
+    .catch((err) => {
+      console.log("신고 수 차트 에러");
+    });
+
+    // axios.post("http://localhost:8080/chart/qna", {
+
+    // })
+    // .then((res)=>{})
+    // .catch((err)=>{})
+
   }, []);
 
   return (
@@ -69,7 +103,8 @@ export default function ManageMain() {
           <Chart data={NewSignIn} type="line" />
         </div>
         <div className="HalfChart">
-
+          <Chart data={ReportNum} type="bar" />
+          {/* <Chart data={QNANum} type="bar" /> */}
         </div>
       </main>
     </div>
