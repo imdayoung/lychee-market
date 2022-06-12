5c43006aeb24d5679362f63c364471eabf5c135e
mouse0429 <mouse0429@naver.com>
2022-06-13 02:18:09 +0900

    mypage text-overflow fix
    mypage text-overflow fix

--- src/pages/MyPage/components/Product.js
+++ src/pages/MyPage/components/Product.js
@@ -7,7 +7,7 @@ export default function Product(props) {
       <div id="MyPageProduct">
         <img id="MyPageProductImg" alt="상품이미지" src={props.Img == null ? "images/products/prod00.png" : props.Img}></img>
         <div id="MyPageProductDetail">
-          <div>{props.Title}</div>
+          <div id="MyPageProductTitle">{props.Title}</div>
           <div className="OnlyFontBold">
             {Common.MoneyComma(props.Amount)}원
           </div>
