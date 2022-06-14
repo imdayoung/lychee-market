c603cff2110318b0dbc4408416123fa3f3fd3174
다영 <86220363+imdayoung@users.noreply.github.com>
2022-06-14 22:17:18 +0900

    평가 Modal CSS 수정

--- src/pages/Evaluate/EvaluateModal.js
+++ src/pages/Evaluate/EvaluateModal.js
@@ -41,11 +41,11 @@ const EvaluateModal = (props) => {
             <div id="EvaluateModalText"><span id="EvaluateModalTextStrong">{id}</span>님, <span id="EvaluateModalTextStrong">{yourid}</span>님과의 거래가 완료되었습니다!</div>
             <div id="EvaluateModalText">거래는 어떠셨나요?</div>
             <div id="ScoreSelect">
-                <label><input type="radio" className="Invisible" name="evaluate" value="best" onChange={RadioHandler}></input><Score icon={'/images/evaluate/best.png'} title={'최고예요'}></Score></label>
-                <label><input type="radio" className="Invisible" name="evaluate" value="good" onChange={RadioHandler}></input><Score icon={'/images/evaluate/good.png'} title={'좋아요'}></Score></label>
-                <label><input type="radio" className="Invisible" name="evaluate" value="soso" onChange={RadioHandler}></input><Score icon={'/images/evaluate/soso.png'} title={'보통이예요'}></Score></label>
-                <label><input type="radio" className="Invisible" name="evaluate" value="bad" onChange={RadioHandler}></input><Score icon={'/images/evaluate/bad.png'} title={'별로예요'}></Score></label>
-                <label><input type="radio" className="Invisible" name="evaluate" value="worst" onChange={RadioHandler}></input><Score icon={'/images/evaluate/worst.png'} title={'최악이예요'}></Score></label>
+                <input type="radio" className="Invisible" name="evaluate" value="best" id="best" onChange={RadioHandler}></input><label htmlFor="best"><Score icon={'/images/evaluate/best.png'} title={'최고예요'}></Score></label>
+                <input type="radio" className="Invisible" name="evaluate" value="good" id="good" onChange={RadioHandler}></input><label htmlFor="good"><Score icon={'/images/evaluate/good.png'} title={'좋아요'}></Score></label>
+                <input type="radio" className="Invisible" name="evaluate" value="soso" id="soso" onChange={RadioHandler}></input><label htmlFor="soso"><Score icon={'/images/evaluate/soso.png'} title={'보통이예요'}></Score></label>
+                <input type="radio" className="Invisible" name="evaluate" value="bad" id="bad" onChange={RadioHandler}></input><label htmlFor="bad"><Score icon={'/images/evaluate/bad.png'} title={'별로예요'}></Score></label>
+                <input type="radio" className="Invisible" name="evaluate" value="worst" id="worst" onChange={RadioHandler}></input><label htmlFor="worst"><Score icon={'/images/evaluate/worst.png'} title={'최악이예요'}></Score></label>
             </div>
         </main>
           <footer>
