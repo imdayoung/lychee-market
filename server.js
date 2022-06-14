9843cabdb6f5183b2c2b8931e31e6040179f5e9c
다영 <86220363+imdayoung@users.noreply.github.com>
2022-06-14 22:24:15 +0900

    메인페이지 최근 업로드 상품 10개 제한

--- server.js
+++ server.js
@@ -1113,7 +1113,7 @@ app.post('/newproduct', function(req, res) {
 
 /* 전체 상품 불러오기 */
 app.get('/all', function(req, res) {
-	var SQL = "SELECT deal_type, product_id, product_title, product_price, product_img FROM `PRODUCT` ORDER BY `product_date` DESC"
+	var SQL = "SELECT deal_type, product_id, product_title, product_price, product_img FROM `PRODUCT` ORDER BY `product_date` DESC LIMIT 10;"
 	db.query(SQL, (err, result) => {
 		if(err) {
 			console.log("상품 불러오기 오류", err);
